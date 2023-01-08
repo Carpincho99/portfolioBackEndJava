836bc72317462ca8ef4990abfe9a09ee23dfad76
Carpincho99 <ezegiorgis@hotmail.com>
2023-01-08 19:21:46 -0300

    change controller for RestController notation

--- src/main/java/com/portfolio/backEndArgProg/Controller/EducController.java
+++ src/main/java/com/portfolio/backEndArgProg/Controller/EducController.java
@@ -17,9 +17,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @RequestMapping("/educ")
 @CrossOrigin(origins = "https://portfolioargprogramafrontend.web.app")
 public class EducController {
--- src/main/java/com/portfolio/backEndArgProg/Controller/ExpController.java
+++ src/main/java/com/portfolio/backEndArgProg/Controller/ExpController.java
@@ -9,7 +9,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +17,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/exp")
 @CrossOrigin(origins = "https://portfolioargprogramafrontend.web.app")
 public class ExpController {
